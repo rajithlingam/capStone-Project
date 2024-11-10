6256e8398d97d6f44c2cc6201b299a6d1d941ad5
rajithlingam <rajithlingamedu@gmail.com>
2024-11-11 01:59:42 +0530

    .gitignore

--- cSP-5_Blog/index.js
+++ cSP-5_Blog/index.js
@@ -12,28 +12,28 @@ app.use(methodOverride("_method")); // Add method-override middleware
 let data = {}; // Store data here temporarily
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+  res.render("index.ejs");
 });
 
 app.post("/submit", (req, res) => {
-    data = {
-        opp: req.body.getsay,
-        opau: req.body.getAuthor,
-        oph: req.body.getHeading,
-    };
-    res.render("app.ejs", data);
+  data = {
+    opp: req.body.getsay,
+    opau: req.body.getAuthor,
+    oph: req.body.getHeading,
+  };
+  res.render("app.ejs", data);
 });
 
 app.put("/", (req, res) => {
-    // Handle updating logic here, currently just redirects to index
-    res.redirect("/");
+  // Handle updating logic here, currently just redirects to index
+  res.redirect("/");
 });
 
 app.delete("/delete", (req, res) => {
-    data = { opp: "", opau: "", oph: "" }; // Clear the data
-    res.render("app.ejs", data); // Re-render the app.ejs with cleared data
+  data = { opp: "", opau: "", oph: "" }; // Clear the data
+  res.render("app.ejs", data); // Re-render the app.ejs with cleared data
 });
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
